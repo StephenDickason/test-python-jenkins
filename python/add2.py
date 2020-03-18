ee8b92cb988d24639f9e327b34070e1f43f4e4b2
StephenDickason <42997973+StephenDickason@users.noreply.github.com>
2020-03-18 14:05:51 +0000

    Add files via upload

---
+++ python/add2.py
@@ -0,0 +1,16 @@
+import os 
+import sys
+
+def add2(num1, num2):
+    result = num1 - num2
+    return result
+
+def displayResult(num):
+    print(num)
+
+def test_add2():
+    assert add2(2,2) == 4
+
+if __name__ == "__main__":
+    number = add2(3,2)
+    displayResult(number)
